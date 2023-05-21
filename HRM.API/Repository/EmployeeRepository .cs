1501ad1343abd4ff9848d22c12e3f391d074c595
Mahesh Jada <mjada@innominds.com>
2023-05-21 09:45:51 +0530

    Add project files.

---
+++ HRM.API/Repository/EmployeeRepository .cs
@@ -0,0 +1,42 @@
+﻿using HRM.API.Db;
+using HRM.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRM.API.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly HRMContext _context;
+
+        public EmployeeRepository(HRMContext context)
+        {
+            _context = context;
+        }
+
+        public Task<string> DeleteEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> SaveEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Employee>> SelectAllEmployees()
+        {
+            var allemployess = _context.Employees.ToListAsync();
+            return await allemployess;
+        }
+
+        public Task<Employee> SelectEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateEmployee(int id, Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HRM.API/Services/EmployeeService .cs
@@ -0,0 +1,40 @@
+﻿using HRM.API.Model;
+using HRM.API.Repository;
+
+namespace HRM.API.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository repository)
+        {
+            _employeeRepository = repository;
+        }
+
+        public Task<string> AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> EditEmployee(int id, Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return  await _employeeRepository.SelectAllEmployees();
+        }
+
+        public Task<string> RemoveEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
